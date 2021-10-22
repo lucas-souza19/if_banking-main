9604b5d53747072af9a78273a66c0db15d9fa00b
lucas-souza19 <lucassouzacps19@gmail.com>
2021-10-21 22:26:25 -0300

    Correção

--- code/Factory/Cliente.java
+++ code/Factory/Cliente.java
@@ -3,6 +3,7 @@ package Factory;
 public class Cliente {
     public static void main(String[] args) {
         String tipoConta;
+        
 		Conta corrente = FabricaConta.criarConta(tipoConta = "Conta Corrente");
         System.out.println(corrente.getTipoConta());
 
